1b52f5784e282cd0dedcba303dab3e9b6835dee3
Claudio Chies <chiesc@ethz.ch>
2024-01-17 13:50:15 +0100

    added uXRCE and snowsampler with launchfile to Systemd (untested)

--- systemd/configure.sh
+++ systemd/configure.sh
@@ -14,5 +14,10 @@ PACKAGE_PATH=/home/user/ros2_ws/src/adaptive-snowsampler
 # Configure systemd service
 echo "Copy systemd service"
 cp -vf $PACKAGE_PATH/systemd/adaptive-snowsampler.service /etc/systemd/system/
+cp -vf $PACKAGE_PATH/systemd/microxrce_agent.service /etc/systemd/system/
 
 systemctl enable adaptive-snowsampler.service
+systemctl enable microxrce_agent.service
+
+sudo systemctl start adaptive-snowsampler.service
+sudo systemctl start microxrce_agent.service
--- systemd/run_controller.sh
+++ systemd/run_controller.sh
@@ -3,4 +3,4 @@
 set -e
 
 source $HOME/ros2_ws/install/setup.bash
-ros2 launch adaptive_snowsampler adaptive_snowsampler.launch
+ros2 launch adaptive_snowsampler launch_nuc.xml
