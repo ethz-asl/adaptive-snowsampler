48312b6d133bc68f4a03d027c5ccd3101306fc0b
Claudio Chies <61051109+Claudio-Chies@users.noreply.github.com>
2024-02-02 16:52:05 +0100

    Bugfix corrupted .mp4 files (#27)
    * bugfix of corrupted .mp4 recordings

--- systemd/run_record.sh
+++ systemd/run_record.sh
@@ -7,4 +7,3 @@ gst-launch-1.0 -e rtspsrc location=rtsp://127.0.0.1:8553/stream protocols=tcp ! 
 
 source $HOME/ros2_ws/install/setup.bash
 ros2 bag record -o ~/rosbag/bag_$(date +%s) /snowsampler/landing_leg_angle /target_slope /target_normal /tf /fmu/out/vehicle_global_position /fmu/out/vehicle_attitude /elevation_map
-
